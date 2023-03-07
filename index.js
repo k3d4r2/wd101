6c9a9c13065e36c7c0bf9b476d27e7db746598b4
Kedar Salunkhe <92195955+k3d4r2@users.noreply.github.com>
2023-03-07 22:55:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,110 +1,73 @@
-let element = (id) => document.getElementById(id);
+let userForm = document.getElementById("user-form");
+var UserEntries = [];
 
-let classes = (classes) => document.getElementsByClassName(classes);
-
-let user_entries = [];
-
-function writeTable(){
-    let obj = sessionStorage.getItem("user_entries");
-    if(obj){
-        user_entries = JSON.parse(obj);
-    }else{
-        user_entries = [];
-    }
-    return user_entries;
-}1
-user_entries = writeTable();
-
-let username = element("name"),
-  email = element("email"),
-  password = element("password"),
-  tc = element("tc"),
-  dob = element("dob");
-
-let errormsg = classes("errormsg");
-
-let form = element("form");
-
-function verify(elem,message,cnd){
-    if(cnd){
-        elem.style.border = "2px solid red";
-        elem.setCustomValidity(message);
-        elem.reportValidity();
-    }else{
-        elem.style.border = "2px solid green";
-        elem.setCustomValidity('');
-
-    }
-}
-
-function checkDOB(){
-    let age = new Date().getFullYear() - new Date(dob.value).getFullYear();
-    if(age < 18 || age>55){
-        return false;
-    }else{
-        return true;
-    }
-}
-
-let email_err_message = "Not a valid E-mail";
-let tc_err_message = "You must agree to the terms and conditions";
-let dob_err_message = "Your age must be between 18 and 55 to continue";
-
-
-tc.addEventListener("input", (e) => {
-    let cond_agree = !tc.checked;
-    e.preventDefault();
-    verify(tc,tc_err_message,cond_agree);
-});
-
-function createObj(){
-    let check = false;
-    if(tc.checked){
-        check = true;
-    }
-    let obj = {
-        name: username.value,
-        email: email.value,
-        password: password.value,
-        dob: dob.value,
-        checked: check
-    }
-    return obj;
-}
-
-
-function showTable(){
-    let table = element("user-table");
-    let entries = user_entries;
-    let str = `<tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Password</th>
-                    <th>Dob</th>
-                    <th>Accepted terms?</th>
-                </tr>\n`;
-    for(let i=0;i<entries.length;i++){
-        str += `<tr>
-                    <td>${entries[i].name}</td>
-                    <td>${entries[i].email}</td>
-                    <td>${entries[i].password}</td>
-                    <td>${entries[i].dob}</td>
-                    <td>${entries[i].checked}</td>
-                </tr>\n`;
-    }
-    table.innerHTML = str;
-}
-
-form.addEventListener("submit", (e) => {
-    let cond_agree= !tc.checked;
-    e.preventDefault();
-    if (!cond_agree) {
-        let obj = createObj();
-        user_entries.push(obj);
-        sessionStorage.setItem("user_entries", JSON.stringify(user_entries));
-    }
-    showTable();
-});
-window.onload = (event) => {
-    showTable();
+let errors = [];
+const retieveEntries = () => {
+  let entries = localStorage.getItem("UserEntries");
+  if (entries) {
+    entries = JSON.parse(entries);
+  } else {
+    entries = [];
+  }
+  return entries;
 };
+const displayEntries = () => {
+  let entries = retieveEntries();
+  const tbleEntries = entries
+    .map((entry) => {
+      const nameCell = `<td class='border px-5 py-2'>${entry.name}</td>`;
+      const emailCell = `<td class='border px-5 py-2'>${entry.email}</td>`;
+      const passwordCell = `<td class='border px-5 py-2'>${entry.password}</td>`;
+      const dobCell = `<td class='border px-5 py-2'>${entry.dob}</td>`;
+      const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptTerms}</td>`;
+      const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+      return row;
+    })
+    .join("\n");
+  const table = ` <table class='table-auto w-full'>
+    <tr>
+    <th class='px-4 py-2 '>Name </th>
+    <th class='px-4 py-2 '>Email </th>
+    <th class='px-4 py-2 '>Password </th>
+    <th class='px-4 py-2 '>Dob </th>
+    <th class='px-4 py-2 '>Accepted terms? </th>
+    </tr>${tbleEntries}
+</table>`;
+  let details = document.getElementById("user-entries");
+  details.innerHTML = table;
+};
+
+const saveUserForm = (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const dob = document.getElementById("dob").value;
+  const acceptTerms = document.getElementById("acceptTerms").checked;
+  var currentYear = new Date().getFullYear();
+  var birthYear = dob.split("-");
+  let year = birthYear[0];
+  var age = currentYear - year;
+  console.log({ age, currentYear, birthYear });
+  if (age < 18 || age > 55) {
+    document.getElementById("dob").style = "border:1px solid red";
+    return alert("Your age must be under 18 and 55 years");
+  } else {
+    document.getElementById("dob").style = "border:none";
+
+    const entry = {
+      name,
+      email,
+      password,
+      dob,
+      acceptTerms,
+    };
+    UserEntries = retieveEntries();
+    UserEntries.push(entry);
+    localStorage.setItem("UserEntries", JSON.stringify(UserEntries));
+    displayEntries();
+    userForm.reset();
+  }
+};
+userForm.addEventListener("submit", saveUserForm);
+displayEntries();
